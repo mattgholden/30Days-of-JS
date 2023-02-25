1c5cbc3fe3189693b7fc50cb87a89f0718e8553a
mgholden <mgholden27@gmail.com>
2023-02-25 15:04:11 -0500

    JS is working in HTML file

--- 2023.02.25-Day2_JavaScript30/Clock.js
+++ 2023.02.25-Day2_JavaScript30/Clock.js
@@ -1,12 +1,12 @@
 
-const secondHand = document.querySelector('.second-hand')
-function setDate() {
-    // console.log('Hello');
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360);
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
-}
+// const secondHand = document.querySelector('.second-hand')
+// function setDate() {
+//     // console.log('Hello');
+//     const now = new Date();
+//     const seconds = now.getSeconds();
+//     const secondsDegrees = ((seconds / 60) * 360);
+//     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+//     console.log(seconds);
+// }
 
-// setInterval(setDate, 1000);
+// // setInterval(setDate, 1000);
